f25dbeb0984fd7cf6dd2bb59b02f8a823a9aa542
Snehal <snehal.satardekar@ambab.com>
2017-12-26 15:42:22 +0530

    added mlab url

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongojs = require( 'mongojs' );
 
 exports.connectToServer = function( callback ) {
-    return mongojs( "mongodb://localhost:27017/sss_latest_16_02_17");
+    // return mongojs( "mongodb://localhost:27017/sss_latest_16_02_17");
+    return mongojs( "mongodb://heroku_783879j6:99bh0d7ck7nu239cve4k2ajffc@ds163806.mlab.com:63806/heroku_783879j6");
 };
