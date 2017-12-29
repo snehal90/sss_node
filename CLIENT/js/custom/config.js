369a9d28aa2f263cfe43adacf2f32c24e8119085
Snehal <snehal.satardekar@ambab.com>
2017-12-29 14:15:11 +0530

    changed api url variable value for frontend

--- CLIENT/js/custom/config.js
+++ CLIENT/js/custom/config.js
@@ -1,5 +1,5 @@
 var app = angular.module('config', []);
 app.value('CONFIGS', {
-	'api_url' : 'http://localhost:3000/api/v1/',
+	'api_url' : '/api/v1/',
 	'secret_key' : 'dBBmapwEBWUkcUg7xP8Buvp6vc36truv'
 });
